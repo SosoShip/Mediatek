ef55d49b29583b9dd82b5b416d64de5f834db547
SosoShip <verducci.sonia.cloe@gmail.com>
2024-02-05 22:21:40 +0100

    Initial commit

---
+++ src/SVE.Mediatek.ViewModel/ConnectionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVE.Mediatek.ViewModel
+{
+    public class ConnectionViewModel
+    {
+        public string LblMediatek { get; set; }
+        public string LblTitle { get; set; }
+        public string lblInformation { get; set; }
+        public string lblMail { get; set; }
+        public string lblPassword { get; set; }
+        public string btnValidate { get; set; }
+
+        public ConnectionViewModel() 
+        {
+            LblMediatek = "MEDIATEK";
+            LblTitle = "GESTION DU PERSONNEL";
+            lblInformation = "Se connecter pour continer";
+            lblMail = "EMAIL";
+            lblPassword = "MOT DE PASSE";
+            btnValidate = "Se connecter";
+        }
+    }
+}
---
+++ src/SVE.Mediatek/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using SVE.Mediatek.View;
+using SVE.Mediatek.ViewModel;
+
+namespace SVE.Mediatek
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Opens the login window.
+        /// </summary>
+        protected override void OnStartup(StartupEventArgs e)
+        {           
+            base.OnStartup(e);
+            var connectionWindow = new Connection();
+            // Binding of the connection window's view-viewModel
+            connectionWindow.DataContext = new ConnectionViewModel();
+            connectionWindow.Show();
+        }
+    }
+
+}
